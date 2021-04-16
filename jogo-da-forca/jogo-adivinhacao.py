5eb994fe371de19c64a140eef717df814bd64434
tukpedro <pastukpedro@gmail.com>
2021-04-15 23:53:00 -0300

    Jogo de adivinhãção de palavras

--- jogo-da-forca/jogo-adivinhacao.py
+++ jogo-da-forca/jogo-adivinhacao.py
@@ -1,12 +1,12 @@
 import random
 
-secret_words = [
+palavras_secretas = [
 
     'perfume', 'abacate', 'estrume', 'candelabro', 'gaguejar', 'infinito', 'pantera', 'instinto', 'pelejar', 'rabanete'
     'lagosta', 'interruptor', 'maturidade', 'pescador', 'labirinto', 'eletrocardiograma', 'extinto', 'abobrinha'
 
 ]
-secreto = secretos[random.randint(0, len(secretos)-1)]
+palavra_selecionada = palavras_secretas[random.randint(0, len(palavras_secretas)-1)]
 digitado = []
 
 while True:
@@ -18,7 +18,7 @@ while True:
 
     digitado.append(letra)
 
-    if letra in secreto:
+    if letra in palavra_selecionada:
         print('Opa! Existe essa letra na palavra! ;)\n')
     else:
         print('Não existe esta letra na palavra :(\n')
@@ -26,21 +26,21 @@ while True:
 
     secreto_temp = ''
 
-    for letra_secreta in secreto:
+    for letra_secreta in palavra_selecionada:
         if letra_secreta in digitado:
             secreto_temp += letra_secreta
         else:
             secreto_temp += '*'
 
-    if secreto_temp == secreto:
-        print(f'GANHOU! Palavra secreta: {secreto}')
+    if secreto_temp == palavra_selecionada:
+        print(f'GANHOU! Palavra secreta: {palavra_selecionada}')
         sair = input('\nDeseja sair? [s]im ou [n]ao: ')
 
         if sair == 's':
             break
         else:
             print()
-            secreto = secretos[random.randint(0, len(secretos)-1)]
+            palavra_selecionada = palavras_secretas[random.randint(0, len(palavras_secretas)-1)]
             digitado = []
     else:
         print(secreto_temp)
